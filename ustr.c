eb2efb526acbdc769240c306475e0d2f94e800ce
antspham <165956921+antspham@users.noreply.github.com>
2025-07-23 12:22:01 -0700

    Added subtring

--- ustr.c
+++ ustr.c
@@ -30,6 +30,15 @@ int32_t len(UStr s) {
 	return s.codepoints;
 }
 
+int utf8CharLen(const char *p) {
+        unsigned char c = (unsigned char)*p;
+        if ((c & 0x80) == 0x00) return 1;
+        if ((c & 0xE0) == 0xC0) return 2;
+        if ((c & 0xF0) == 0xE0) return 3;
+        if ((c & 0xF8) == 0xF0) return 4;
+        return 1;
+}
+
 /*
 Returns a substring of str, starting at index start (inclusive) 
 and ending at index end (exclusive).
@@ -37,8 +46,59 @@ and ending at index end (exclusive).
 Returns an empty string on invalid range.
 */
 UStr substring(UStr s, int32_t start, int32_t end) {
-	// TODO: implement this
+	UStr result = {0};
+        if (start < 0 || end < 0 ||start > end || end > s.codepoints || !s.contents) {
+                return new_ustr("");
+        }
 
+        if (s.is_ascii == 1) {
+                int32_t len = end - start;
+                char* copy = malloc(len + 1);
+                if (!copy) {
+                        return new_ustr("");
+                }
+                memcpy (copy, s.contents + start, len);
+                copy[len] = '\0';
+                result = new_ustr(copy);
+                free(copy);
+                return result;
+        }
+
+        char* p = s.contents;
+        int32_t idx = 0;
+        char* byteStart = NULL;
+        char* byteEnd = NULL;
+        while (*p) {
+                if (idx == start) {
+                        byteStart = p;
+                }
+
+                if (idx == end) {
+                        byteEnd = p;
+                        break;
+                }
+                p += utf8CharLen(p);
+                idx++;
+        }
+
+        if (byteStart == NULL) {
+                return new_ustr("");
+        }
+
+        if (byteEnd == NULL) {
+                byteEnd = p;
+        }
+
+        int32_t byteLen = byteEnd - byteStart;
+        char* copy = malloc (byteLen + 1);
+        if (!copy) {
+                return new_ustr("");
+        }
+        memcpy(copy, byteStart, byteLen);
+        copy[byteLen] = '\0';
+        result = new_ustr(copy);
+        free(copy);
+        return result;
 }
 
 /*
